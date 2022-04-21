4d6645678831e0ed292389eae7b51f0707f756a8
Vijal Patel <vijalp@otobit.in>
2022-04-21 18:10:57 +0530

    21-04

--- Models/Employee/Staff/BankDetail/AccountDetail.cs
+++ Models/Employee/Staff/BankDetail/AccountDetail.cs
@@ -9,6 +9,7 @@ namespace HIsabKaro.Models.Employee.Staff.BankDetail
     public class AccountDetail
     {
         public string Name { get; set; }
+        [RegularExpression(@"^[0-9]{9,18}$", ErrorMessage = "Only Numabers or 9 to 18 Digit Number!")]
         public string AccountNumber { get; set; }
         public string ConAccountNumber { get; set; }
         public string IFSC { get; set; }
--- Models/Employee/Staff/SatffEmploymentDetail.cs
+++ Models/Employee/Staff/SatffEmploymentDetail.cs
@@ -11,9 +11,9 @@ namespace HIsabKaro.Models.Employee.Staff
         public DateTime DOJ { get; set; }
         public string Designation { get; set; }
         public string Department { get; set; }
-        [RegularExpression(@"^[0-9]{12}$", ErrorMessage = "Only Numaber, 12 Digit Number!")]
+        [RegularExpression(@"^[0-9]{12}$", ErrorMessage = "UAN Only Numabers or 12 Digit Number!")]
         public string UAN { get; set; }
-        [RegularExpression(@"^[0-9]{17}$", ErrorMessage = "Only Numaber, 17 Digit Number!")]
+        [RegularExpression(@"^[0-9]{17}$", ErrorMessage = "ESI Only Numabers or 17 Digit Number!")]
         public string ESI { get; set; }
         [RegularExpression(@"^[A-Z]{5}\d{4}[A-Z]{1}$", ErrorMessage = "Invalid Pan Card Number!")]
         public string PAN { get; set; }
