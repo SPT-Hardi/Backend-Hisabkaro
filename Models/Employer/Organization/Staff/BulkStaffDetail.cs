4a6f4f8ff6178d9f2e2f2fab7b552202ef4b7119
Vijal Patel <vijalp@otobit.in>
2022-05-03 10:29:38 +0530

    03/05

---
+++ Models/Employer/Organization/Staff/BulkStaffDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HIsabKaro.Models.Employer.Organization.Staff
+{
+    public class BulkStaffDetail
+    {
+        public string CSV { get; set; }
+    }
+    //public class student
+    //{
+    //    public string Name { get; set; }
+    //    public string Age { get; set; }
+    //    public string MobileNumber { get; set; }
+
+    //}
+}
