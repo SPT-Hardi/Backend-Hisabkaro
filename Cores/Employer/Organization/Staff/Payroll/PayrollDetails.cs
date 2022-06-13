43058dc00ea58de6326bed548b0616891c5d8f3f
Vijal Patel <vijalp@otobit.in>
2022-06-13 16:35:17 +0530

    11/06

---
+++ Cores/Employer/Organization/Staff/Payroll/PayrollDetails.cs
@@ -0,0 +1,25 @@
+﻿using HIsabKaro.Models.Common;
+using HisabKaroContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace HIsabKaro.Cores.Employer.Organization.Staff.Payroll
+{
+    public class PayrollDetails
+    {
+        public Result PFCreate(object URId, List<Models.Employer.Organization.Staff.Payroll.PayrollDetail> value)
+        {
+            using (DBContext c = new DBContext())
+            {
+                using (TransactionScope scope = new TransactionScope())
+                {
+                    var p = (from x in c.PayrollFixedLookups
+                             select x).ToList();
+                }
+            }
+        } 
+    }
+}
